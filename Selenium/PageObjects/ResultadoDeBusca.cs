42a491fd9df0d51572b0c5b47b34eeab858bddda
Lazaro Fernandes <lazaro.fernandes@lambda3.com.br>
2017-03-17 17:06:22 -0300

    estrutura selenium

---
+++ Selenium/PageObjects/ResultadoDeBusca.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using OpenQA.Selenium;
+namespace Selenium.PageObjects
+{
+    public class ResultadoDeBusca
+    {
+        public static string ObterLinkDeResultado(IWebDriver driver, int numeroDoResultado)
+        {
+            var primeiroResultado = driver.FindElement(By.CssSelector($"#rso > div:nth-child(1) > div > div:nth-child({numeroDoResultado}) > div > h3 > a"));
+            return primeiroResultado.GetAttribute("href");
+        }
+
+        public static object ObterTituloDaPagina(IWebDriver driver)
+        {
+            System.Threading.Thread.Sleep(2000);
+            return driver.Title;
+        }
+    }
+}
