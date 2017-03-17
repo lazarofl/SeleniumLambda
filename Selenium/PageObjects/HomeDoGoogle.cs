5cd0aabb963004678f53fe4bccbfe9886702fdec
Lazaro Fernandes <lazaro.fernandes@lambda3.com.br>
2017-03-17 16:56:39 -0300

    Estrutura do projeto Selenium

---
+++ Selenium/PageObjects/HomeDoGoogle.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+
+namespace Selenium.PageObjects
+{
+    public class HomeDoGoogle
+    {
+        public static void Pesquisar(IWebDriver driver, string pesquisa)
+        {
+            driver.Url = "https://www.google.com.br/";
+            driver.FindElement(By.Id("lst-ib")).SendKeys(pesquisa);
+            driver.FindElement(By.Id("lst-ib")).Submit();
+
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(2);
+        }
+    }
+}
---
+++ Selenium/TestesDeBuscaNoGoogle.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+using Selenium.PageObjects;
+
+namespace Selenium
+{
+    [TestClass]
+    public class TestesDeBuscaNoGoogle
+    {
+        [TestMethod]
+        public void DeveEncontrarOBlogDoLazaroNoPrimeiroResultadoDaPesquisa()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                HomeDoGoogle.Pesquisar(driver, "Lazaro Fernandes Lima Suleiman");
+                var link = ResultadoDeBusca.ObterLinkDeResultado(driver, 1);
+
+                Assert.AreEqual("http://lazarofl.github.io/", link);
+            }
+        }
+
+        [TestMethod]
+        public void DeveEncontrarOLinkedinDoLazaroNoSegundoResultadoDaPesquisa()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                HomeDoGoogle.Pesquisar(driver, "Lazaro Fernandes Lima Suleiman");
+                var link = ResultadoDeBusca.ObterLinkDeResultado(driver, 2);
+
+                Assert.AreEqual("https://br.linkedin.com/in/lazaroflima", link);
+            }
+        }
+
+        [TestMethod]
+        public void DeveTerOValorPesquisaNoTítuloDaPáginadeResultadoDaPesquisa()
+        {
+            using (IWebDriver driver = new ChromeDriver())
+            {
+                HomeDoGoogle.Pesquisar(driver, "Lazaro Fernandes Lima Suleiman");
+                var titulodaPagina = ResultadoDeBusca.ObterTituloDaPagina(driver);
+
+                Assert.AreEqual("Lazaro Fernandes Lima Suleiman - Pesquisa Google", titulodaPagina);
+            }
+        }
+    }
+}
